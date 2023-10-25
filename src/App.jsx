fa49d74771519291099f3720ebed95bc45f84c05
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-25 13:11:35 -0500

    added stuff idk bad commit alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-// import useState from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 import axios from "axios"
 
@@ -19,29 +16,32 @@ class App extends React.Component {
     }
   }
 
-  handleCityFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log('handle submit test')
-    let URL = `http://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.state.cityName}&format=json`;
-    let cityInfo = await axios.get(URL);
-    console.log("city info: ", cityInfo.data[0])
-    this.setState({
-      cityInfo: cityInfo.data[0]
-    });
-  }
-
   handleCityInput = (event) => {
     // console.log('life!')
     this.setState({
       cityName: event.target.value,
+      cityData: null,
     });
   };
+
+  handleCityFormSubmit = async (event) => {
+    event.preventDefault();
+    // console.log('handle submit test');
+    let URL = `http://us1.locationiq.com/v1/search?key=${API_KEY}&q=${this.state.cityName}&format=json`;
+    let cityInfo = await axios.get(URL);
+    // console.log("city info: ", cityInfo.data[0])
+    this.setState({
+      //[0-2]?
+      cityInfo: cityInfo.data[0]
+    });
+  }
+
 
 
   render() {
     console.log(this.state.cityInfo)
+    //declared in handleCityInput, but inside of function
     let cityData = this.state.cityInfo;
-
     let cityList = this.state.cityInfo.map((data, index) => {
       return (
         <li key={index}>
@@ -55,36 +55,49 @@ class App extends React.Component {
     return (
       <>
         <h1>Welcome to City Explorer</h1>
-        <form onSubmit={this.handleSubmit}>
+
+
+        <form onSubmit={this.handleCityFormSubmit}>
+
+          <label>
+            Pick a City:
+            <input type="text" onChange={this.handleCityInput} />
+          </label>
           <button type="submit">Explore!</button>
         </form>
+
         {this.state.error ? (
           <p>this.state.errorMessage</p>
         ) : (
           <ul>{cityList}</ul>
         )}
 
-        <form onSubmit={this.handleCityFormSubmit}>
-          <label>
-            Pick a City:
-            <input type="text" onChange={this.handleCityInput} />
-          </label>
-          <button type="submit">Explore!2</button>
-        </form >
 
-        {this.state.cityInfo && (
-        <>
-        <p>{this.state.cityInfo.display_name}</p>
-        <p>{this.state.cityInfo.lat}</p>
-        <p>{this.state.cityInfo.lon}</p>
-        <image src={this.state.cityInfo.img_url} alt={this.state.cityInfo.display_name} title={this.state/>
-      </>
-      
+
+
+
+     
+
+      {
+        this.state.cityInfo && (
+          <>
+            <p>{this.state.cityInfo.display_name}</p>
+            <p>{this.state.cityInfo.lat}</p>
+            <p>{this.state.cityInfo.lon}</p>
+            {/* <image src={this.state.cityInfo.img_url} alt={this.state.cityInfo.display_name} title={this.state} /> */}
+          </>
+        )
       }
-        {this.state.lat && this.state.lon && this.state.cityInto.img_url &&
-        <img src={this.state.cityInfo.img_url} />}
+    {
+      this.state.lat && this.state.lon && this.state.cityInto.img_url &&
+        <img src={this.state.cityInfo.img_url} />
+    }
+
+    {
+      <p>The weather in {this.cityInfo.display_name} will be a low of {this.state.cityInfo.low_temp} and a high of {this.state.cityInfo.max_temp}, and conditions will be {this.state.cityInfo.</p>
+    }
       </>
-    );
+    )
   }
 }
 
