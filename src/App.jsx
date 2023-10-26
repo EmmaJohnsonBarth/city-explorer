660c55a399d682a5a970bc02fc3bfc48cd62a5d9
EMMA JOHNSON-BARTH <emma.johnson9013@gmail.com>
2023-10-25 21:50:11 -0500

    attempted to add accordion

--- src/App.jsx
+++ src/App.jsx
@@ -38,48 +38,48 @@ class App extends React.Component {
     // console.log('from LOCATIOIQ',cityInfo.data[0])
     //update state and then lets create a component 
     this.setState({
-      location : cityInfo.data[0].display_name,
-      lat : cityInfo.data[0].lat,
-      lon : cityInfo.data[0].lon
+      location: cityInfo.data[0].display_name,
+      lat: cityInfo.data[0].lat,
+      lon: cityInfo.data[0].lon
     });
-    this.handleWeather(cityInfo.data[0].lat,cityInfo.data[0].lon)
+    this.handleWeather(cityInfo.data[0].lat, cityInfo.data[0].lon)
   }
 
 
   // add an arrow function to call your server with the weather route 
   // we need for weather is the lat and lon from location IQ
-handleWeather = async (lat, lon) => {
-   console.log(lat, lon, 'did we get here?');
-   try {
-    //          call server  /   route
-    let weatherFromServer = await axios.get( `${VITE_SERVER_URL}/weather`, {
-      params: {
-        lat: lat,
-        lon: lon,
-        searchQuery: this.state.cityName
-      } 
-    });
-    console.log('weather',weatherFromServer.data);
-    //update state, create a weather component to send state via props to display weather data. 
-    const weatherData = weatherFromServer.data;
-    this.setState({
-      weather:weatherData,
-    })
+  handleWeather = async (lat, lon) => {
+    console.log(lat, lon, 'did we get here?');
+    try {
+      //          call server  /   route
+      let weatherFromServer = await axios.get(`${VITE_SERVER_URL}/weather`, {
+        params: {
+          lat: lat,
+          lon: lon,
+          searchQuery: this.state.cityName
+        }
+      });
+      console.log('weather', weatherFromServer.data);
+      //update state, create a weather component to send state via props to display weather data. 
+      const weatherData = weatherFromServer.data;
+      this.setState({
+        weather: weatherData,
+      })
 
-    // updatestate with the weather
-    // from state give the weather to the weather component
-   } catch (error) {
-    // console.log('Error fetching weather data: ', error);
-   }
-}
+      // updatestate with the weather
+      // from state give the weather to the weather component
+    } catch (error) {
+      // console.log('Error fetching weather data: ', error);
+    }
+  }
 
   render() {
     // console.log(this.state.lat,  this.state.location);
     return (
       <>
-      <h1> Welcome to City Explore!</h1>
+        <h1> Welcome to City Explore!</h1>
         <form onSubmit={this.handleCityFormSubmit}>
-      
+
           <label>
             Pick a City:
             <input type="text" onChange={this.handleCityInput} />
@@ -91,9 +91,21 @@ handleWeather = async (lat, lon) => {
           `{this.state.errorMessage}`
         ) : (
           <>
-          <p>{this.state.location}</p>
-          <p>{this.state.lat}</p>
-          <p>{this.state.lon}</p>
+            <div className="accordion">
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  {/* <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  View Coordinates of Selected City</button> */}
+                </h2>
+                <div id="collapseOne" className="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                  <div className="accordion-body">
+                    <strong>{this.state.location}</strong>
+                     <p>{this.state.lat}</p>
+                     <p>{this.state.lon}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </>
         )}
       </>
